e07eed772e1e513e75c1e442c6a3302f53d71d6b
BenAppleton <benappleton91@gmail.com>
2024-01-13 13:21:17 +1300

    updates to the blog page

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ document.getElementById('header-hamburger').addEventListener('click', function()
 document.getElementById('footer-hamburger').addEventListener('click', function() {
   document.querySelector('footer .navbar').classList.toggle('active');
 });
+// code for the "Load More" button on the blog page
+document.getElementById('loadMore').addEventListener('click', function() {
+  var row3 = document.getElementById('row3');
+  var progressBar = document.getElementById('progressBar');
+  var loadMoreButton = document.getElementById('loadMore');
+
+  row3.style.display = 'flex';
+  loadMoreButton.style.display = 'none';
+
+  // Update the progress bar to 100%
+  progressBar.style.width = '100%';
+});
