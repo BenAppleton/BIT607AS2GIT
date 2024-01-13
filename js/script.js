6c4b6a82bbb97c6a1e8ff619acf7d4d3672872b2
BenAppleton <benappleton91@gmail.com>
2024-01-13 14:09:18 +1300

    nearly complted blog page

--- js/script.js
+++ js/script.js
@@ -17,3 +17,31 @@ document.getElementById('loadMore').addEventListener('click', function() {
   // Update the progress bar to 100%
   progressBar.style.width = '100%';
 });
+document.getElementById('blogSearch').addEventListener('input', function(e) {
+  var searchTerm = e.target.value.toLowerCase();
+  var blogs = document.querySelectorAll('.blog');
+
+  blogs.forEach(function(blog) {
+      var textContent = blog.innerText.toLowerCase();
+      if (textContent.includes(searchTerm)) {
+          blog.style.display = ''; // Show the blog post
+      } else {
+          blog.style.display = 'none'; // Hide the blog post
+      }
+  });
+});
+document.getElementById('blogFilter').addEventListener('change', function(e) {
+  var selectedTag = e.target.value;
+  var blogs = document.querySelectorAll('.blog');
+
+  blogs.forEach(function(blog) {
+      // Check if the blog contains a div with a class matching the selected tag
+      var hasTag = (selectedTag === 'all') || blog.querySelector('.blog-tag.' + selectedTag);
+
+      if (hasTag) {
+          blog.style.display = ''; // Show the blog post
+      } else {
+          blog.style.display = 'none'; // Hide the blog post
+      }
+  });
+});
