fb669875c3560e8df4cff53b709e3c8f93070134
BenAppleton <benappleton91@gmail.com>
2024-01-16 16:09:51 +1300

    Update to booking form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// >>>> JS for global navigation <<<< //
+// >>>> JS for global navigation <<<<
 document.getElementById('header-hamburger').addEventListener('click', function() {
   document.querySelector('header .navbar').classList.toggle('active');
 });
@@ -7,7 +7,7 @@ document.getElementById('footer-hamburger').addEventListener('click', function()
   document.querySelector('footer .navbar').classList.toggle('active');
 });
 
-// >>>> JS for blog page <<<< //
+// >>>> JS for blog page <<<<
 // code for the "Load More" button on the blog page
 document.getElementById('loadMore').addEventListener('click', function() {
   var row3 = document.getElementById('row3');
